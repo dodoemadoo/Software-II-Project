6b3d2960e3da18d35b53d6a3df8df1e1820b88d6
Andrew Emad <andrewemad614@gmail.com>
2020-04-01 00:19:44 +0200

    main

--- SWESection/src/main/java/com/Swesection/MainController.java
+++ SWESection/src/main/java/com/Swesection/MainController.java
@@ -1,6 +1,5 @@
 package com.Swesection;
 
-import java.sql.SQLException;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +9,6 @@ import com.Connection.SqlConnection;
 @SpringBootApplication
 public class MainController {
 	public static void main(String []args ) throws Exception {
-		//SpringApplication.run(MainController.class, args);
-		SqlConnection con =SqlConnection.getInstance();
-		System.out.print(con.selectData("select * from users"));
-		//System.out.println(con.selectData("select * from Users"));
+		SpringApplication.run(MainController.class, args);
 	}
 }
